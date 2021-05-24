67a32dfc1da7f5bd32b150720dc8dea80ab5c1e8
Lukas Kucerik <kuceriklukas@gmail.com>
2021-05-24 13:06:02 +0200

    Make the api calculate the image sizing options according to the build settings and the incoming image

--- machinelearningteam/image-scaling-service/pkg/api/api.go
+++ machinelearningteam/image-scaling-service/pkg/api/api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"image"
+	_ "image/jpeg"
 	"log"
 
 	api "github.com/kuceriklukas/hiring-assigments/machinelearningteam/image-scaling-service/proto"
@@ -18,13 +21,15 @@ type Server struct {
 }
 
 const (
-	host = "localhost:50052"
+	host          = "localhost:50052"
+	scalingMargin = 5
 )
 
 // ScaleImage echoes the image provides in the request
 func (s *Server) ScaleImage(ctx context.Context, req *api.ScaleImageRequest) (*api.ScaleImageReply, error) {
-	fmt.Println("Received image...")
+	fmt.Println("Received an image")
 
+	// make connection to the image_svc via grpc
 	conn, err := grpc.Dial(host, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -32,15 +37,21 @@ func (s *Server) ScaleImage(ctx context.Context, req *api.ScaleImageRequest) (*a
 	defer conn.Close()
 	client := image_svc.NewImageOptimizerClient(conn)
 
-	image := req.Image.GetContent()
+	// get the images bytes and create an image object
+	imageBytes := req.Image.GetContent()
+	providedImage, _, err := image.Decode(bytes.NewReader(imageBytes))
 
+	if err != nil {
+		fmt.Printf("Error decoding image %+v\n", err.Error())
+	}
+
+	// calculate if we need to scale the image and what the height and width should be
+	scalingOptions := calculateScalingOptions(providedImage, s.TargetWidth, s.TargetHeight)
+
+	// create the response object
 	resp, err := client.OptimizeImage(ctx, &image_svc.OptimizeImageRequest{
-		Image: image,
-		Scale: &image_svc.SizingOptions{
-			Scale:        true,
-			TargetWidth:  int32(s.TargetWidth),
-			TargetHeight: int32(s.TargetHeight),
-		},
+		Image:     imageBytes,
+		Scale:     scalingOptions,
 		Greyscale: s.DoGreyscale,
 	})
 
@@ -51,4 +62,45 @@ func (s *Server) ScaleImage(ctx context.Context, req *api.ScaleImageRequest) (*a
 	return &api.ScaleImageReply{
 		Content: resp.GetContent(),
 	}, nil
+}
+
+func calculateScalingOptions(image image.Image, targetWidth int, targetHeight int) *image_svc.SizingOptions {
+	actualTargetWidth := image.Bounds().Max.X
+	actualTargetHeight := image.Bounds().Max.Y
+	scaleImage := false
+
+	// calculate what the max and min widths and heights are (depending on the scalingMargin in percentage)
+	// since division might end up as a float, we need to careful with the types here, that's why so many conversions
+	widthMax := int(float32(targetWidth) / float32(100) * (float32(100 + scalingMargin)))
+	widthMin := int(float32(targetWidth) / float32(100) * (float32(100 - scalingMargin)))
+	heightMax := int(float32(targetHeight) / float32(100) * (float32(100 + scalingMargin)))
+	heightMin := int(float32(targetHeight) / float32(100) * (float32(100 - scalingMargin)))
+
+	// check if the image width is bigger or smaller than the desired maxWidth or minWidth
+	if image.Bounds().Max.X < widthMin || image.Bounds().Max.X > widthMax {
+		actualTargetWidth = targetWidth
+		scaleImage = true
+
+		// if the image width has been scaled, we need to scale the height of the image as well, so we don't destroy the image's ratio
+		if scaleImage {
+			scalingRatio := float32(targetWidth) / float32(image.Bounds().Max.X)
+			actualTargetHeight = int(float32(image.Bounds().Max.Y) * scalingRatio)
+		}
+	} else if image.Bounds().Max.Y < heightMin || image.Bounds().Max.Y > heightMax {
+		// check if the image height is bigger or smaller than the desired maxHeight or minHeight
+		actualTargetHeight = targetHeight
+		scaleImage = true
+
+		// if the image width has been scaled, we need to scale the width of the image as well, so we don't destroy the image's ratio
+		if scaleImage {
+			scalingRatio := float32(targetHeight) / float32(image.Bounds().Max.Y)
+			actualTargetWidth = int(float32(image.Bounds().Max.X) * scalingRatio)
+		}
+	}
+
+	return &image_svc.SizingOptions{
+		Scale:        scaleImage,
+		TargetWidth:  int32(actualTargetWidth),
+		TargetHeight: int32(actualTargetHeight),
+	}
 }
